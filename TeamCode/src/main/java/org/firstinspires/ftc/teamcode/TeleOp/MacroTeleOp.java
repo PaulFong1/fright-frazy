80f4c3c8ddc3db2c8b5de1f54e5a513c04351855
Aarav Chandrasekar <aaravc@gmail.com>
2022-01-22 15:45:30 -0800

    MacroTeleOp Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MacroTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MacroTeleOp.java
@@ -6,9 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.BaseRobot;
 
 // Created for 16887.
-@TeleOp(name = "Test TeleOp", group = "TeleOp")
+@TeleOp(name = "Macro TeleOp", group = "TeleOp")
 //@Disabled
-public class TestTeleOp extends BaseRobot {
+public class MacroTeleOp extends BaseRobot {
     private int stage = 0;
     private int firstLevel = 0;
     private int secondLevel = 200;
@@ -47,29 +47,29 @@ public class TestTeleOp extends BaseRobot {
     @Override
     public void loop() {
 
-     //  telemetry.addData("actuator:", "pos=%d, power=%.2f, zero=%s, tar=%d", actuator.getCurrentPosition(), actuator.getPower(), actuator.getZeroPowerBehavior(), actuator.getTargetPosition());
+        //  telemetry.addData("actuator:", "pos=%d, power=%.2f, zero=%s, tar=%d", actuator.getCurrentPosition(), actuator.getPower(), actuator.getZeroPowerBehavior(), actuator.getTargetPosition());
         //     reset_drive_encoders();
         //    reset_linearSlide_encoders();
         //    reset_rotate_encoders();
         //     reset_spin_encoders();
         //tankanum_original(gamepad1.right_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
-         tankanum_drive(gamepad1.right_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        tankanum_drive(gamepad1.right_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
 
+        telemetry.addData("your", "mom");
 
-       /* tank_drive(gamepad2.left_stick_y, gamepad2.right_stick_y);
         if (gamepad2.left_bumper)
         {
-            topSpin.setPower(0.5);
+            box_Spin.setPosition(0.5);
         }
         else if (gamepad2.right_bumper)
         {
-            topSpin.setPower(-0.5);
+            box_Spin.setPosition(-0.5);
         }
 
         else {
             topSpin.setPower(0);
-        }*/
-/*
+        }
+
         if (gamepad2.left_trigger > 0.2)
             topSpin.setPower(gamepad1.left_trigger);
 
@@ -88,11 +88,7 @@ public class TestTeleOp extends BaseRobot {
 
         //     }
 
-        if (gamepad1.left_bumper)
-            box_Spin.setPosition(0.4);
 
-        else if (gamepad1.right_bumper)
-            box_Spin.setPosition(0.1);
 
 
 
@@ -105,45 +101,13 @@ public class TestTeleOp extends BaseRobot {
             axle_Spin.setPower(0);
 
 
-        if (get_linearSlide_motor_enc() < secondLevel - 100) stage = 0;
-        else if (get_linearSlide_motor_enc() < thirdLevel - 100) stage = 1;
-        else stage = 2;
+
 
 //        telemetry.addData("lift enc: ", "=%.2f", linearSlide.getCurrentPosition());
 //        telemetry.addData("stage: ", "=%d",stage);
         telemetry.update();
 
-        if (gamepad1.x && stage < 2) {
-            linearSlideSetPosition(linearSlide, stage == 0 ? secondLevel : thirdLevel);
-            stage++;
-        }
 
-        else if (gamepad1.y && stage > 0) {
-            linearSlideSetPosition(linearSlide, stage == 2 ? secondLevel : firstLevel);
-            stage--;
-        }
-
-        else if (gamepad1.dpad_right) {
-            linearSlide.setPower(0.7);
-            if (linearSlide.getCurrentPosition() >= secondLevel) {
-                stage = 1;
-            }
-            if (linearSlide.getCurrentPosition() >= thirdLevel) {
-                stage = 2;
-            }
-        }
-
-        else if (gamepad1.dpad_left) {
-            linearSlide.setPower(-0.1);
-            if (linearSlide.getCurrentPosition() <= secondLevel) {
-                stage = 1;
-            }
-            if (linearSlide.getCurrentPosition() <= firstLevel) {
-                stage = 0;
-            }
-        } else linearSlide.setPower(0);
-
-        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         //turn the motor for the linear slide
@@ -166,23 +130,27 @@ public class TestTeleOp extends BaseRobot {
 
 
         if (gamepad1.dpad_up) {
-// Don't know if RUN_TO_POSITION is better than BRAKE
-//            actuator.setPower(-1);
-//            rot1holdpos = actuator.getCurrentPosition() - 1;  // Each press move the rotate up 10 pos
-            rot1holdpos -= 7;  // Each press move the rotate up 10 pos
-            actuator.setTargetPosition(rot1holdpos);
-            actuator.setPower(-1.0);                           // maximum power to move up and hold
-            actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            while (actuator.isBusy()) { }        // May cause loop problem
-            //rotate2.setPower(0.1);
-        }
-        else if (gamepad1.dpad_down) {
-//            actuator.setPower(0.3);
-//            rot1holdpos = actuator.getCurrentPosition() + 1;
-            rot1holdpos += 5;
-            actuator.setTargetPosition(rot1holdpos);
+            actuator.setTargetPosition(100);
             actuator.setPower(0.5);
             actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            box_Spin.setPosition(0.5);
+            box_Spin.setPosition(0);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            actuator.setTargetPosition(0);
+            actuator.setPower(-0.5);
+            actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        else if (gamepad1.dpad_down) {
+            actuator.setTargetPosition(0);
+            actuator.setPower(-0.5);
+            actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 //            while (actuator.isBusy()) { }
             // rotate2.setPower(0.5);
 
@@ -193,7 +161,7 @@ public class TestTeleOp extends BaseRobot {
         }
 
         else {
-            actuator.setTargetPosition(rot1holdpos);        // Try to stabilize
+            actuator.setTargetPosition(actuator.getCurrentPosition());        // Try to stabilize
             actuator.setPower(-1.0);                        // Need maximize the hold the position
             actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //            while (actuator.isBusy()) { }
@@ -211,7 +179,7 @@ public class TestTeleOp extends BaseRobot {
 
 
 
- */
+
         if (gamepad1.left_stick_button) DEBUG = !DEBUG; // Toggle the debug flag
         super.loop();
 
